9b8ded8e4203e024af454dfdd617cdb0cab06389
warLite <yeretaweh@gmail.com>
2022-12-13 07:08:45 -0800

    .

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <main.h>
+
+int main()
+{
+	printf("_putchar"\n);
+	retun=0;
+
--- 0x02-functions_nested_loops/0-putchar.c0-putchar.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	printf("_putchar\n")
-	return 0;
-}
